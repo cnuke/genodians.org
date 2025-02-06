3d15d892718f6dda82387334e0e336147b8e8ecb
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-02-06 14:13:23 +0100

    fixup 'Run uacme locally to update fullchain.pem'

--- tool/uacme.sh
+++ tool/uacme.sh
@@ -1,28 +1,52 @@
 #!/bin/sh
 
-#SERVER=localhost:5556
-#SERVER=genodians.org
-SERVER=testing.genodians.org
+#
+# The following environment variables must be set prior to invoking
+# this script:
+#
+# UACME_DOMAIN       specifies the domain for which the certificate
+#                    shoult be issed, e.g. genodians.org
+# UACME_PRODUCTION   specifies the intent to use the production server
+#                    rather than the staging one used for testing
+# WEBDAV_USER        specifies the username for WebDAV access
+# WEBDAV_PASSWORD    specifies the password for WebDAV access
+#
+
+if [ -z "$UACME_DOMAIN" ]; then
+	echo "$(basename "$0") requires UACME_DOMAIN environment variable set"
+	exit 1
+fi
 
 # CONFDIR
-#   private/key.pem         account private key
-#   private/SERVER/key.pem  SERVER private key     - privkey.pem
-#   SERVER/cert.pem         certificate for SERVER - fullchain.pem
+#   private/key.pem               account private key
+#   private/UACME_DOMAIN/key.pem  UACME_DOMAIN private key     - privkey.pem
+#   UACME_DOMAIN/cert.pem         certificate for UACME_DOMAIN - fullchain.pem
+FORCE="--force"
 CONFDIR="--confdir uacme.d"
 CRYPTO="--type EC --bits 384"
-STAGING="--staging"
-VERBOSE="--verbose"
+VERBOSE="-v -v -v"
+
+[ -z "$UACME_PRODUCTION" ] && STAGING="--staging" || STAGING=""
+
+check_webdav_credentials() {
+	local prog_name="$1"
+	if [ -z "$WEBDAV_USER" ] || [ -z "$WEBDAV_PASSWORD" ]; then
+		echo "$prog_name requires WEBDAV_USER and WEBDAV_PASSWORD environment variables set"
+		exit 1
+	fi
+}
 
 if [ "$1" = "uacme-issue" ]; then
-	uacme ${FORCE} ${STAGING} ${VERBOSE} ${CONFDIR} ${CRYPTO} --hook $0 issue ${SERVER}
+	check_webdav_credentials "uacme-issue"
+
+	uacme ${FORCE} ${STAGING} ${VERBOSE} ${CONFDIR} ${CRYPTO} \
+	      --hook $0 issue ${UACME_DOMAIN} www.${UACME_DOMAIN}
 	exit $?
 fi
 
 if [ "$1" = "user-conf" ]; then
-	if [ -z "$WEBDAV_USER" ] || [ -z "$WEBDAV_PASSWORD" ]; then
-		echo "user-conf requires WEBDAV_USER and WEBDAV_PASSWORD environment variables set"
-		exit 1
-	fi
+	check_webdav_credentials "user-conf"
+
 	echo $WEBDAV_USER:upload:$(echo -n $WEBDAV_USER:upload:$WEBDAV_PASSWORD | sha256sum | head -c 64)
 	exit $?
 fi
@@ -60,50 +84,48 @@ fi
 
 #echo "--- METHOD=${METHOD} TYPE=${TYPE} IDENT=${IDENT} TOKEN=${TOKEN} AUTH=${AUTH}"
 
-WEBDAV_USER=user
-WEBDAV_PASSWORD=pw
-WEBDAV="curl --insecure --digest --user $WEBDAV_USER:$WEBDAV_PASSWORD"
+WEBDAV="curl --verbose --insecure --digest --user $WEBDAV_USER:$WEBDAV_PASSWORD"
 
 case "$METHOD" in
 	"begin")
 		printf "%s" "${AUTH}" > TOKEN
-		$WEBDAV --upload-file TOKEN https://${SERVER}/.well-known/acme-challenge/${TOKEN}
+		$WEBDAV --upload-file TOKEN https://${UACME_DOMAIN}/upload/acme-challenge/${TOKEN}
 		exit $?
 		;;
 
 	"done")
 		rm TOKEN
-		$WEBDAV --request DELETE https://localhost:5556/.well-known/${TOKEN}
-		$WEBDAV --upload-file uacme.d/${SERVER}/cert.pem https://${SERVER}/upload/cert/fullchain.pem
+		$WEBDAV --request DELETE https://${UACME_DOMAIN}/upload/acme-challenge/${TOKEN}
+		$WEBDAV --upload-file uacme.d/${UACME_DOMAIN}/cert.pem https://${UACME_DOMAIN}/upload/cert/fullchain.pem
 		exit $?
 		;;
 
 	"failed")
 		rm TOKEN
-		$WEBDAV --request DELETE https://localhost:5556/.well-known/${TOKEN}
+		$WEBDAV --request DELETE https://${UACME_DOMAIN}/upload/acme-challenge/${TOKEN}
 		exit $?
 		;;
 
 	*)
-		echo "$0: invalid method" 1>&2 
+		echo "$0: invalid method" 1>&2
 		exit 1
 esac
 
 #
 # Notes
 #
-# openssl s_client -connect ${SERVER}:443 -servername ${SERVER} | openssl x509 -noout -dates
+# openssl s_client -connect ${UACME_DOMAIN}:443 -servername ${UACME_DOMAIN} | openssl x509 -noout -dates
 # openssl rsa  -in privkey.pem   -text -noout
 # openssl x509 -in fullchain.pem -text -noout
 # openssl req  -new -x509 -keyout privkey.pem -out fullchain.pem -days 365 -nodes
 #
 # uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR new [EMAIL]
 # uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR update [EMAIL]
-# uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR --hook ./uacme.sh issue ${SERVER}
-# uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR revoke ${SERVER}
+# uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR --hook ./uacme.sh issue ${UACME_DOMAIN}
+# uacme [--staging] --verbose --type EC --bits 384 --confdir $CONFDIR revoke ${UACME_DOMAIN}/cert.pem
 #
-# curl --insecure --digest --user user:pw --upload-file TOKEN         https://${SERVER}/upload/acme-challenge/
-# curl --insecure --digest --user user:pw --upload-file fullchain.pem https://${SERVER}/upload/cert/
-# curl --insecure --digest --user user:pw --request DELETE            https://${SERVER}/upload/acme-challenge/TOKEN
+# curl --insecure --digest --user user:pw --upload-file TOKEN         https://${UACME_DOMAIN}/upload/acme-challenge/
+# curl --insecure --digest --user user:pw --upload-file fullchain.pem https://${UACME_DOMAIN}/upload/cert/
+# curl --insecure --digest --user user:pw --request DELETE            https://${UACME_DOMAIN}/upload/acme-challenge/TOKEN
 #
 # echo user:upload:$(echo -n user:upload:pw | sha256sum | head -c 64) > upload-user.conf
